f7f7983b18350b74f9f33c8a94ad8b9fbb7ab539
Roi <2243253+oceansize@users.noreply.github.com>
2021-11-26 13:53:10 +0100

    Update spec_helper.rb
    Removes reference to 'Bookmark Manager'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,7 +35,7 @@ ENV['ENVIRONMENT'] = 'test'
 # Bring in the contents of the `app.rb` file. The below is equivalent to: require_relative '../app.rb'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
-# Tell Capybara to talk to BookmarkManager
+# Tell Capybara to talk to Chitter
 Capybara.app = Chitter
 
 RSpec.configure do |config|
